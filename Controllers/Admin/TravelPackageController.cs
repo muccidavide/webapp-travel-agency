8f7e8c18716706108b359db65501852629d83f06
muccidavide <davidemucci0804@gmail.com>
2022-10-20 16:02:02 +0200

    Complete CRUD admin

--- Controllers/Admin/TravelPackageController.cs
+++ Controllers/Admin/TravelPackageController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 using System.Security.Cryptography.Xml;
 using webapp_travel_agency.Data;
@@ -63,6 +64,8 @@ namespace webapp_travel_agency.Controllers.Admin
             {
                 return View(travelData);
             }
+
+            travelData.TravelPackage.Tags = _ctx.Tags.Where(ingredient => travelData.SelectedTags.Contains(ingredient.Id)).ToList<Tag>();
             try
             {
                 _ctx.TravelPackages.Add(travelData.TravelPackage);
@@ -74,6 +77,78 @@ namespace webapp_travel_agency.Controllers.Admin
                 return View("Create", travelData);
             }
             return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            TravelPackage travel = _ctx.TravelPackages.Where(t => t.Id == id).FirstOrDefault();
+
+            if(travel == null)
+            {
+                return NotFound();
+            }
+
+            _ctx.TravelPackages.Remove(travel);
+            _ctx.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        
+        public IActionResult Update(int id)
+        {
+
+            TravelPackage travelToUpdate = _ctx.TravelPackages.Where(t => t.Id == id).Include(t => t.Transport).Include(t => t.Category).Include(t => t.Destination).Include(t => t.Tags).FirstOrDefault();
+
+            if(travelToUpdate == null)
+            {
+                return NotFound();
+            }
+
+            travelSupportModel.TravelPackage = travelToUpdate;
+            return View(travelSupportModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(int id, TravelSupportModel travelData)
+        {
+            TravelPackage travelToUpdate = _ctx.TravelPackages.Where(t => t.Id == id).Include(t => t.Transport).Include(t => t.Category).Include(t => t.Destination).Include(t => t.Tags).FirstOrDefault();
+
+            if (travelToUpdate == null)
+            {
+                return NotFound();
+            }
+
+            travelToUpdate.Name = travelData.TravelPackage.Name;
+            travelToUpdate.Description = travelData.TravelPackage.Description;
+            travelToUpdate.CoverImage = travelData.TravelPackage.CoverImage;
+            travelToUpdate.Description = travelData.TravelPackage.Description;
+            travelToUpdate.DepartureDate = travelData.TravelPackage.DepartureDate;
+            travelToUpdate.ReturnDate = travelData.TravelPackage.ReturnDate;
+            travelToUpdate.Price = travelData.TravelPackage.Price;
+            travelToUpdate.CategoryId = travelData.TravelPackage.CategoryId;
+            travelToUpdate.TransportId = travelData.TravelPackage.TransportId;
+            travelToUpdate.DestinationId = travelData.TravelPackage.DestinationId;
+            travelToUpdate.Tags = _ctx.Tags.Where(tag => travelData.SelectedTags.Contains(tag.Id)).ToList<Tag>();
+
+            try
+            {
+                _ctx.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("StoreDataExcetipn", ex.Message);
+               
+                return View("Update", travelData);
+
+            }
+
+            return RedirectToAction("Index");
+
+
+            
         }
 
     }
